0ec2bff6f7b437755c9f1812dc946d805c52ca5f
josiepetch <nichakorn-p@live.com>
2024-08-17 11:59:34 +1000

    modify vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/excel_to_graph/",
+  base: "/react-excel_to_graph/",
   plugins: [react()],
 })
